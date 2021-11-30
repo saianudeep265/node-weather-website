4a093f36161930118cda115bc281086b9b7a4b07
Anudeep <lingadan@MacBook-Pro.local>
2021-11-30 12:51:54 +0530

    Init commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,20 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1Ijoic2FpYW51ZGVlcDI2NSIsImEiOiJja3Z6MXMyNTljdjZsMnVzN3F3cWlqbG11In0.6jmIIjg36TvT3sxgEJhrRw&limit=1'
+    request({ url, json: true}, (error, { body }) => {
+       if (error) {
+          callback('Unable to connect to mapbox service', undefined)
+       } else if (body.features.length === 0) {
+          callback('Unable to find the location!', undefined)
+       } else {
+          callback(undefined, {
+             latitude: body.features[0].center[1],
+             longitude: body.features[0].center[0],
+             location: body.features[0].place_name
+          })
+       }
+    })
+ }
+
+module.exports = geocode
